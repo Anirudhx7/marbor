bed15b257a5c534441a544dc2d7186dbb5d054cc
anirudhx7 <anirudh.official07@gmail.com>
2026-08-22 21:24:31 +0530

    fix: mobile drawer used 100vh - theme toggle unreachable behind phone browser chrome

--- ui/src/components/Sidebar.tsx
+++ ui/src/components/Sidebar.tsx
@@ -214,7 +214,7 @@ export function Sidebar({ onLogout, session, pendingCount = 0 }: SidebarProps) {
       {/* Sidebar - desktop always visible, mobile slide-in */}
       <aside
         className={`
-          fixed left-0 top-0 z-50 w-64 bg-card border-r border-border flex flex-col h-screen
+          fixed left-0 top-0 z-50 w-64 bg-card border-r border-border flex flex-col h-screen supports-[height:100dvh]:h-dvh
           transition-transform duration-300 ease-in-out
           ${isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}
         `}
